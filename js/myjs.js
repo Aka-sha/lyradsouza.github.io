004159014f66be8a3a695006884eccc6168bafae
Lyra D'Souza <dsouzalyra937@gmail.com>
2020-08-22 23:57:13 -0700

    Added additonal files. All page-changing buttons have functionality. Just need backend for log in and application.

---
+++ js/myjs.js
@@ -0,0 +1,7 @@
+
+
+/*var aboutButton = document.getElementById("about-button");
+function clickButton(){
+    document.body.innerHTML="Aboot";
+};
+aboutButton.addEventListener("click", clickButton);*/
